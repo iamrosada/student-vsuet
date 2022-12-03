df9c2e18d7892a7dada90799a80fa8374e293768
iamrosada <luisrosada2018@gmail.com>
2022-12-03 20:23:11 +0300

    feat: updated the project

--- src/schema/makeExecutableSchema/makeExecutable.schema.ts
+++ src/schema/makeExecutableSchema/makeExecutable.schema.ts
@@ -1,7 +1,7 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { merge } from "lodash";
 
-import { typeDefs as student } from "../schema.student";
+import { typeDefs as student } from "../student/schema.student";
 import { resolvers as resolverStudent } from "../../resolvers/resolvers.student";
 
 export const schemas = makeExecutableSchema({
